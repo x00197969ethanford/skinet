e5dd4d5d1de9a7130001383b9414161e91ac9084
Ethan <x00197969@mytudublin.ie>
2025-07-18 12:08:20 +0100

    real

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     prodction: true,
     apiUrl: 'api/',
     hubUrl: 'hub/notifications',
-    stripePublicKey: 'pk_test_51RWxt84PvyvSCwtty366YoBbrdVI851bvTjDXgP0eTwvvk4bZPel72CMVn3IjUwyhsVnzNaLf6cItzcbtkZ91SIY00i7IV58TR'
+    stripePublicKey: 'pk_test_51RjjFEIczWxSA1GoGNKP4g19Kz2scXJw1rGt7eXIeFxyvdgl3JstoHF0hiXR4K8Mb5JjxxJJx2zWI0IKUeh6nWDu00EDHyMNyb'
 };
